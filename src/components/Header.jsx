baf948453ec834caa0bc773ef12300712314cc77
Vaibhav6403 <vaibhavchauhan807@gmail.com>
2024-04-18 18:41:25 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="py-1 border-b-2 border-b-gray-300 drop-shadow-md fixed top-0 inset-x-0 bg-white mb-3">
       <h1 className="font-bold text-xl uppercase text-center">
-        Codehelp Blogs
+        insazy Blogs
       </h1>
     </header>
   );
